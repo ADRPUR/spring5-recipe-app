2086df0c1b05c9be327e36b81d68ea7f6cfbeb0f
ADRPUR <apurice@gmail.com>
2020-12-10 19:06:57 +0200

    Initial commit

---
+++ src/test/java/md/purice/Spring5RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package md.purice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5RecipeAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
