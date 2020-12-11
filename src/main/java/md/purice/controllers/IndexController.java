b9157afbb038147a2076abe7b93bbba1b9e06fc9
ADRPUR <apurice@gmail.com>
2020-12-11 21:42:36 +0200

    spring data jpa query methods

--- src/main/java/md/purice/controllers/IndexController.java
+++ src/main/java/md/purice/controllers/IndexController.java
@@ -1,13 +1,35 @@
 package md.purice.controllers;
 
+import md.purice.domain.Category;
+import md.purice.domain.UnitOfMeasure;
+import md.purice.repositories.CategoryRepository;
+import md.purice.repositories.UnitOfMeasureRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 public class IndexController {
 
+    private CategoryRepository categoryRepository;
+    private UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public IndexController(CategoryRepository categoryRepository,
+                           UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
     @RequestMapping({"","/", "index"})
     public String getIndexPage(){
+
+        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
+        Optional<UnitOfMeasure> unitOfMeasureOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+
+        System.out.println("Cat id is: " + categoryOptional.get().getId());
+        System.out.println("Uom id is: " + unitOfMeasureOptional.get().getId());
+
         return "index";
     }
 }
