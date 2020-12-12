8431a8b18ffc8d7c09a18751cd9129161302d2e7
ADRPUR <apurice@gmail.com>
2020-12-12 22:59:35 +0200

    refactor with Lombok project

--- src/main/java/md/purice/bootstrap/RecipeBootstrap.java
+++ src/main/java/md/purice/bootstrap/RecipeBootstrap.java
@@ -1,5 +1,6 @@
 package md.purice.bootstrap;
 
+import lombok.extern.slf4j.Slf4j;
 import md.purice.domain.*;
 import md.purice.repositories.CategoryRepository;
 import md.purice.repositories.RecipeRepository;
@@ -8,11 +9,13 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Component
 public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEvent> {
 
@@ -27,6 +30,7 @@ public class RecipeBootstrap implements ApplicationListener<ContextRefreshedEven
     }
 
     @Override
+    @Transactional
     public void onApplicationEvent(ContextRefreshedEvent event) {
         recipeRepository.saveAll(getRecipes());
     }
