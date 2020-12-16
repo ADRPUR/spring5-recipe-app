7c5109874cd161193aef5eeffd98560da00fe1cc
ADRPUR <apurice@gmail.com>
2020-12-16 16:50:09 +0200

    create ingredient

--- src/main/java/md/purice/controllers/IngredientController.java
+++ src/main/java/md/purice/controllers/IngredientController.java
@@ -2,6 +2,8 @@ package md.purice.controllers;
 
 import lombok.extern.slf4j.Slf4j;
 import md.purice.commands.IngredientCommand;
+import md.purice.commands.RecipeCommand;
+import md.purice.commands.UnitOfMeasureCommand;
 import md.purice.services.IngredientService;
 import md.purice.services.RecipeService;
 import md.purice.services.UnitOfMeasureService;
@@ -40,6 +42,27 @@ public class IngredientController {
                                        @PathVariable String id, Model model){
         model.addAttribute("ingredient", ingredientService.findByRecipeIdAndIngredientId(Long.valueOf(recipeId), Long.valueOf(id)));
         return "recipe/ingredient/show";
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{recipeId}/ingredient/new")
+    public String newIngredient(@PathVariable String recipeId, Model model){
+
+        //make sure we have a good id value
+        RecipeCommand recipeCommand = recipeService.findCommandById(Long.valueOf(recipeId));
+        //todo raise exception if null
+
+        //need to return back parent id for hidden form property
+        IngredientCommand ingredientCommand = new IngredientCommand();
+        ingredientCommand.setRecipeId(Long.valueOf(recipeId));
+        model.addAttribute("ingredient", ingredientCommand);
+
+        //init uom
+        ingredientCommand.setUom(new UnitOfMeasureCommand());
+
+        model.addAttribute("uomList",  unitOfMeasureService.listAllUoms());
+
+        return "recipe/ingredient/ingredientform";
     }
 
     @GetMapping
--- src/main/java/md/purice/domain/UnitOfMeasure.java
+++ src/main/java/md/purice/domain/UnitOfMeasure.java
@@ -1,18 +1,20 @@
 package md.purice.domain;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
-@Data
+@Getter
+@Setter
 @Entity
 public class UnitOfMeasure {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    public String description;
+    private String description;
 }
